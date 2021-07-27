7936685433950a28a6f9338b3ee3e4be3ce9bedf
copokopy4ka <copokopy4ka@gmail.com>
2021-07-27 17:11:27 +0300

    added slider to reviews

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ $(function() {
         nav.toggleClass("show");
     });
 
+    // reviews: https://kenwheeler.github.io/slick/
 
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
 
 });
